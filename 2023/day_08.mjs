2dad19314b324c63a029af1c1347e368b8700cc7
Milan Fox <milan.fox@diva-e.com>
2023-12-08 10:04:28 +0100

    23/08 - Haunted Wasteland

---
+++ 2023/day_08.mjs
@@ -0,0 +1,32 @@
+import fs from 'fs';
+
+let [instructions, mapping] = fs.readFileSync('input.txt', 'utf-8').split("\n\n").filter(Boolean)
+mapping = mapping.split("\n").filter(Boolean).reduce((acc, curLine) => {
+  const [dest, L, R] = curLine.match(/[A-Z0-9]{3}/g);
+  return dest ? { ...acc, [dest]: { L, R } } : acc;
+}, {});
+
+const calculateSteps = (start, target) => {
+  let i = 0;
+  const loop = {counter: 0, next: start};
+  while (true) {
+    loop.counter++;
+    loop.next = mapping[loop.next][instructions[i]];
+    i = (i + 1) % instructions.length
+    if (loop.next.endsWith(target)) break;
+  }
+  return loop.counter;
+}
+
+console.log(`Part 1: ${calculateSteps("AAA", "ZZZ")}`)
+
+const allNeededSteps = Object.keys(mapping).filter(key => key.endsWith("A")).map(start => calculateSteps(start, "Z"));
+
+/* Recursive logic shamelessly stolen from https://www.30secondsofcode.org/js/s/lcm/ */
+const getLCM = (...arr) => {
+  const getGreatestCommonDivisor = (x, y) => (!y ? x : getGreatestCommonDivisor(y, x % y));
+  const getLeastCommonMultiple = (x, y) => (x * y) / getGreatestCommonDivisor(x, y);
+  return [...arr].reduce((a, b) => getLeastCommonMultiple(a, b));
+};
+
+console.log(`Part 2: ${ getLCM(...allNeededSteps)}`)
