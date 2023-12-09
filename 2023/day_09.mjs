e5e6c581828be850944fbb973c80ae5a74d258f2
Milan Fox <milan.fox@diva-e.com>
2023-12-09 10:16:14 +0100

    23/09 - Mirage Maintenance

--- 2023/day_09.mjs
+++ 2023/day_09.mjs
@@ -16,29 +16,22 @@ const breakDownSequence = (seq) => {
   return steps;
 };
 
-const findNextValue = (seq) => {
+const continueSequence = (seq, direction) => {
   const structure = breakDownSequence(seq).toReversed();
+  const _continue = {
+    next: (element, i) => {structure[i+1] = [...structure, element.at(-1) + structure[i + 1].at(-1)]},
+    prev: (element, i) => {structure[i+1] = [structure[i+1].at(0) - element.at(0) , ...structure[i+1]]}
+  }
+
+  const position = { next: -1, prev: 0 };
+
   structure.forEach((element, i) => {
     if(i === structure.length - 1) return;
-    structure[i+1].push(element.at(-1) + structure[i+1].at(-1))
+    _continue[direction](element, i);
   })
-  return structure.at(-1).at(-1);
+
+  return structure.at(-1).at(position[direction]);
 }
 
-const allNextValues = inputData.map(seq => findNextValue(seq));
-
-
-console.log(`Part 1: ${allNextValues.reduce(sum)}`);
-
-const findPrevValue = (seq) => {
-  const structure = breakDownSequence(seq).toReversed();
-  structure.forEach((element, i) => {
-    if(i === structure.length - 1) return;
-    structure[i+1] = [structure[i+1].at(0) - element.at(0) , ...structure[i+1]];
-  })
-  return structure.at(-1).at(0);
-}
-
-const allPrevValues = inputData.map(seq => findPrevValue(seq));
-
-console.log(`Part 2: ${allPrevValues.reduce(sum)}`);
+console.log(`Part 1: ${inputData.map(seq => continueSequence(seq, "next")).reduce(sum)}`);
+console.log(`Part 2: ${inputData.map(seq => continueSequence(seq, "prev")).reduce(sum)}`);
