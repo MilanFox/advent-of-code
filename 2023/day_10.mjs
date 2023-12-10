a85b3d61bcc31908c20ed10a7170cad4e4f3ca57
Milan Fox <milan.fox@diva-e.com>
2023-12-10 22:35:01 +0100

    23/10 - Pipe Maze

--- 2023/day_10.mjs
+++ 2023/day_10.mjs
@@ -3,10 +3,10 @@ import fs from 'fs';
 const inputData = fs.readFileSync('input.txt', 'utf-8').split('\n').filter(Boolean).map(line => line.split(''));
 
 const directions = {
-  'north': { opposite: "south", offsetX: 0, offsetY: -1},
-  'east': { opposite: "west", offsetX: 1, offsetY: 0},
-  'south': { opposite: "north", offsetX: 0, offsetY: 1},
-  'west': { opposite: "east", offsetX: -1, offsetY: 0}
+  'north': { opposite: 'south', offsetX: 0, offsetY: -1},
+  'east': { opposite: 'west', offsetX: 1, offsetY: 0},
+  'south': { opposite: 'north', offsetX: 0, offsetY: 1},
+  'west': { opposite: 'east', offsetX: -1, offsetY: 0}
 };
 
 const pipes = {
@@ -25,7 +25,7 @@ const nodes = [];
 let lastVisited = undefined;
 
 while (true) {
-  const {char, x, y} = nodes.at(-1) || {char: "S", x: startX, y: startY};
+  const {char, x, y} = nodes.at(-1) || {char: 'S', x: startX, y: startY};
   if (char === 'S' && nodes.length) break;
 
   for (let i = 0; i < pipes[char].connectsTo.length; i++) {
@@ -47,26 +47,26 @@ while (true) {
 console.log(`Part 1: ${nodes.length / 2}`);
 
 /* Visualize the loop, until I find out what to do with part 2... because why not. */
-const visualizationData = Array.from({ length: 140 }, () => Array(140).fill(" "));
-const speakingCharacters = {'S': '★', '|': '│', '-': "─", 'L': "╰", 'J': "╯", '7': "╮", 'F': "╭"};
+const visualizationData = Array.from({ length: 140 }, () => Array(140).fill(' '));
+const speakingCharacters = {'S': '★', '|': '│', '-': '─', 'L': '╰', 'J': '╯', '7': '╮', 'F': '╭'};
 nodes.forEach(({ char, y, x }) => visualizationData[y][x] = speakingCharacters[char]);
 const generateVisualisation = (data) => data.map(line => line.join('')).join('\n');
-fs.writeFileSync("visualization.txt", generateVisualisation(visualizationData), { flag: "w+" });
+fs.writeFileSync('visualization.txt', generateVisualisation(visualizationData), { flag: 'w+' });
 
 /* Part 2 */
 const upscaleMatrix = (matrix) => {
   const upscaleMap = {
-    " ": [[" ", " ", " "], [" ", "╳", " "], [" ", " ", " "]],
-    "★": [["╭", "─", "╮"], ["│", "★", "│"], ["╰", "─", "╯"]],
-    '│': [[" ", "│", " "], [" ", "│", " "], [" ", "│", " "]],
-    "─": [[" ", " ", " "], ["─", "─", "─"], [" ", " ", " "]],
-    "╰": [[" ", "│", " "], [" ", "╰", "─"], [" ", " ", " "]],
-    "╯": [[" ", "│", " "], ["─", "╯", " "], [" ", " ", " "]],
-    "╮": [[" ", " ", " "], ["─", "╮", " "], [" ", "│", " "]],
-    "╭": [[" ", " ", " "], [" ", "╭", "─"], [" ", "│", " "]],
+    ' ': [[' ', ' ', ' '], [' ', '╳', ' '], [' ', ' ', ' ']],
+    '★': [['╭', '─', '╮'], ['│', '★', '│'], ['╰', '─', '╯']],
+    '│': [[' ', '│', ' '], [' ', '│', ' '], [' ', '│', ' ']],
+    '─': [[' ', ' ', ' '], ['─', '─', '─'], [' ', ' ', ' ']],
+    '╰': [[' ', '│', ' '], [' ', '╰', '─'], [' ', ' ', ' ']],
+    '╯': [[' ', '│', ' '], ['─', '╯', ' '], [' ', ' ', ' ']],
+    '╮': [[' ', ' ', ' '], ['─', '╮', ' '], [' ', '│', ' ']],
+    '╭': [[' ', ' ', ' '], [' ', '╭', '─'], [' ', '│', ' ']],
   }
 
-  const upscaledMatrix = Array.from({ length: matrix.length * 3 }, () => Array(matrix[0].length * 3).fill("."));
+  const upscaledMatrix = Array.from({ length: matrix.length * 3 }, () => Array(matrix[0].length * 3).fill('.'));
   matrix.forEach((row, indexY) => {
     row.forEach((cell, indexX) => {
       for (let i = 0; i < 3; i++) {
@@ -87,8 +87,8 @@ const floodFill = (matrix, posY, posX) => {
   while(fillStack.length > 0) {
     const [posY, posX] = fillStack.pop();
     if (!matrix[posY]) continue;
-    if (matrix[posY][posX] !== " " && matrix[posY][posX] !== "╳" ) continue;
-    // Overwriting " " with "&nbsp;", so we don't run into infinite loops while still looking like a space in the render
+    if (matrix[posY][posX] !== ' ' && matrix[posY][posX] !== '╳' ) continue;
+    // Overwriting ' ' with '&nbsp;', so we don't run into infinite loops while still looking like a space in the render
     matrix[posY][posX] = ' ';
     for (const direction in directions) {
       fillStack.push([posY + directions[direction].offsetY, posX + directions[direction].offsetX])
@@ -98,10 +98,10 @@ const floodFill = (matrix, posY, posX) => {
 
 const upscaledMatrix = upscaleMatrix(visualizationData);
 floodFill(upscaledMatrix, 0 , 0);
-const charactersInLoop = upscaledMatrix.reduce((acc, row) => acc + row.filter(cell => cell === "╳").length, 0);
+const charactersInLoop = upscaledMatrix.reduce((acc, row) => acc + row.filter(cell => cell === '╳').length, 0);
 
 console.log(`Part 2: ${charactersInLoop}`);
 
 /* Visualizing the second part too, because why not - need to zoom out to see it properly.
    Also best seen in browser, since it displays &nbsp; as space character  */
-fs.writeFileSync("visualization2.txt", generateVisualisation(upscaledMatrix), { flag: "w+" });
+fs.writeFileSync('visualization2.txt', generateVisualisation(upscaledMatrix), { flag: 'w+' });
