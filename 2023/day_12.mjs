174d4da2eaeb37fb7d14cee44d705a001ceb8e09
Milan Fox <milan.fox@diva-e.com>
2023-12-12 14:12:28 +0100

    23/12 - Hot Springs

--- 2023/day_12.mjs
+++ 2023/day_12.mjs
@@ -6,32 +6,24 @@ inputData.forEach(line => line[1] = line[1].split(',').map(n => parseInt(n, 10))
 const areAllGroupsValid = ( sequence, condition ) =>
   sequence.split(/\./g).filter(Boolean).every((group, index) => group.length === condition[index]);
 
-
 const isNumberOfGroupsCorrect = ( sequence, condition ) =>
   sequence.split(/\./g).filter(Boolean).length === condition.length;
 
-
 const findAllSolutions = ([record, condition]) => {
   const matches = [];
-
   const continueSequence = (sequence = "", nextChar = "") => {
     if (nextChar) sequence += nextChar;
-
     for (let i = sequence.length; i < record.length; i++) {
       const nextChar = record[i];
-
       if (nextChar === "?") {
         if (areAllGroupsValid(sequence + ".", condition)) continueSequence(sequence, ".");
         continueSequence(sequence, "#");
         return;
       }
-
       sequence += nextChar;
     }
-
     if (areAllGroupsValid(sequence, condition) && isNumberOfGroupsCorrect(sequence, condition)) matches.push(sequence);
   }
-
   continueSequence()
   return matches.length;
 }
@@ -42,7 +34,6 @@ console.log(`Part 1: ${possibleSolutions.reduce((acc, cur) => acc + cur, 0)}`);
 const unfoldedData = inputData.map(([record, condition]) =>
   [Array.from({ length: 5 }, () => record).join("?"), Array.from({ length: 5 }, () => [...condition]).flat()]
 );
-
 
 /* ToDo: Find solution for part 2 which doesn't run until infinity... */
 const possibleSolutionsForUnfoldedData = unfoldedData.map(findAllSolutions);
