9fc117d6ce6015a88723e716c34dfb6a183074a2
Milan Fox <milan.fox@diva-e.com>
2023-12-15 16:11:42 +0100

    23/15 - Lens Library

--- 2023/day_15.mjs
+++ 2023/day_15.mjs
@@ -7,22 +7,22 @@ const getHash = (string) => [...string].reduce((acc, cur) => ((acc + cur.charCod
 console.log(`Part 1: ${inputData.map(getHash).reduce(sum)}`);
 
 const boxes = Array.from({length: 256}, () => []);
-inputData.map(step => {
-    const [label, focalLength] = step.split(/[=-]/).filter(Boolean);
-    const boxIndex = getHash(label);
-    const lensIndex = boxes[boxIndex].findIndex(lens => lens[0] === label);
-    if (focalLength !== undefined) {
-      if (lensIndex < 0) boxes[boxIndex].push([label, focalLength])
-      else boxes[boxIndex][lensIndex][1] = focalLength;
-      return;
-    }
-    if (lensIndex >= 0) boxes[boxIndex].splice(lensIndex, 1);
-  });
+inputData.forEach(step => {
+  const [label, focalLength] = step.split(/[=-]/).filter(Boolean);
+  const boxIndex = getHash(label);
+  const lensIndex = boxes[boxIndex].findIndex(lens => lens[0] === label);
+  if (focalLength !== undefined) {
+    if (lensIndex < 0) boxes[boxIndex].push([label, focalLength])
+    else boxes[boxIndex][lensIndex][1] = focalLength;
+    return;
+  }
+  if (lensIndex >= 0) boxes[boxIndex].splice(lensIndex, 1);
+});
 
 const focussingPower = boxes
   .map((box, boxIndex) => box.length
     ? box.map(([_, focalLength], lensIndex) => (boxIndex + 1) * (lensIndex + 1) * focalLength).reduce(sum)
-    : undefined )
+    : undefined)
   .filter(Boolean)
   .reduce(sum);
 
