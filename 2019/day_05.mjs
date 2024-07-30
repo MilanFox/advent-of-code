03d317b74e86910bc93445b29f23b597447908d9
Milan Fox <milan.fox@diva-e.com>
2024-07-30 14:26:19 +0200

    2019/05 - Sunny with a Chance of Asteroids

--- 2019/day_05.mjs
+++ 2019/day_05.mjs
@@ -4,5 +4,4 @@ import { IntCodeComputer } from './utils/useIntCodeComputer.mjs';
 const memory = fs.readFileSync('input.txt', 'utf-8').trim().split(',').map(n => parseInt(n));
 
 const computer = new IntCodeComputer(memory);
-
 await computer.run();
