bfaa374119af1ca57028f042de9d9bcd104b8cde
Milan Fox <milan.fox@diva-e.com>
2024-07-30 21:22:07 +0200

    2019/07 - Amplification Circuit

--- 2019/day_07.mjs
+++ 2019/day_07.mjs
@@ -1,8 +1,51 @@
 import fs from 'fs';
 import { IntCodeComputer } from './utils/useIntCodeInterpreter.mjs';
 
-const memory = fs.readFileSync('testInput.txt', 'utf-8').trim().split(',').map(n => parseInt(n));
+const memory = fs.readFileSync('input.txt', 'utf-8').trim().split(',').map(n => parseInt(n));
 
-const amplifiers = Array.from({ length: 5 }, () => new IntCodeComputer(memory));
+const generatePermutations = (arr) => {
+  const results = [];
 
-console.log(amplifiers);
+  const permute = (subArr, m = []) => {
+    if (subArr.length === 0) {
+      results.push(m);
+    } else {
+      for (let i = 0; i < subArr.length; i++) {
+        let curr = subArr.slice();
+        let next = curr.splice(i, 1);
+        permute(curr.slice(), m.concat(next));
+      }
+    }
+  };
+
+  permute(arr);
+  return results;
+};
+
+const findPowerLevel = async (inputSequence) => {
+  let last = 0;
+
+  for (let i = 0; i < 5; i++) {
+    const computer = new IntCodeComputer(memory, { input: [inputSequence.shift(), last] });
+    await computer.run();
+    last = computer.lastOutput;
+  }
+
+  return last;
+};
+
+const findHighestPowerLevel = async () => {
+  const permutations = generatePermutations([0, 1, 2, 3, 4]);
+  let tempHighestPowerLevel = 0;
+
+  for (const phaseSetting of permutations) {
+    const powerlevel = await findPowerLevel(phaseSetting);
+    tempHighestPowerLevel = Math.max(tempHighestPowerLevel, powerlevel);
+  }
+
+  return tempHighestPowerLevel;
+};
+
+const highestPowerLevel = await findHighestPowerLevel();
+
+console.log(`Part 1: ${highestPowerLevel}`);
